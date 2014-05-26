25150cebdf2824f17fe0d43195bd251db979b780
Deca Service <you@example.com>
2014-05-26 20:42:02 +0700

    ?

---
+++ sites/all/modules/custom/excur_offer/theme/ob-offer-offer-form.tpl.php
@@ -0,0 +1,29 @@
+<div class="excur-offer-offer-form">
+  <div class="head">
+    <?php print $form['#data']['title']; ?>
+  </div>
+  <div class="details">
+    <div class="row">
+      <div class="span3 info">
+        <?php print $form['#data']['time']; ?>
+        <br />
+        <?php print t('Duration') . ': ' . $form['#data']['duration']; ?>
+      </div>
+      <div class="span3">
+        <?php print render($form['tickets']); ?>
+      </div>
+      <div class="span3">
+        <?php print render($form['date']); ?>
+      </div>
+    </div>
+  </div>
+  <div class="row">
+    <div class="span7"></div>
+    <div class="span2 right">
+      <?php print render($form['submit']); ?>
+    </div>
+  </div>
+  <div class="element-hidden">
+    <?php print drupal_render_children($form); ?>
+  </div>
+</div>
