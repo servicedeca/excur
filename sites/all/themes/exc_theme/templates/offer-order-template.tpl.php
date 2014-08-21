f3675141c3aaf9dec9d0a88d4db96b74af624585
Kiselev Dmitry <kiselev.kala4ek@gmail.com>
2014-08-22 00:54:43 +0700

    Code refactor

--- sites/all/themes/exc_theme/templates/offer-order-template.tpl.php
+++
@@ -1,3 +0,0 @@
-<h1> <?php print t('Your contact details'); ?></h1>
-<?php print render($form['name']); ?>
-<?php print drupal_render_children($form); ?>
---
+++ sites/all/themes/exc_theme/templates/order-form.tpl.php
@@ -0,0 +1,5 @@
+<h1>
+  <?php print t('Your contact details'); ?>
+</h1>
+<?php print render($form['name']); ?>
+<?php print drupal_render_children($form); ?>
