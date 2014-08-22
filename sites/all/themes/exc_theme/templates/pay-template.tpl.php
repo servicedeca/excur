a1acff970e8d758dfaf42786aa6b1dde4fc4fb34
Deca Service <you@example.com>
2014-08-22 16:09:14 +0700

    process

--- sites/all/themes/exc_theme/templates/pay-template.tpl.php
+++ sites/all/themes/exc_theme/templates/pay-template.tpl.php
@@ -1,13 +1,27 @@
-<?php print render($image);?>
-<h1><?php print $offer['title'];?></h1>
-<h2><?php print $offer['id'];?></h2>
+<div class="span3">
+  <?php print render($image);?>
+</div>
+<div class="span5">
+  <h1>
+    <?php print $offer['title'];?>
+  </h1>
+</div>
+<div class="span5">
+  <h2>
+   <?php print t('Order number');?> <?php print $offer['id'];?>
+  </h2>
+</div>
+<div class="span5" id="guide_image">
+  <?php print render($guide_image);?>
+</div>
+
 <h2><?php print $offer['language'];?></h2>
 <h2><?php print $offer['offer'];?></h2>
 <h3><?php print $offer['date'];?></h3>
 <h3><?php print $offer['duration'];?></h3>
 <h3><?php print $offer['city_name'];?></h3>
 <h3><?php print $offer['country_name'];?></h3>
-<?php print render($guide_image);?>
+
 <h3><?php print $offer['guide_name'];?></h3>
 <h3><?php print $offer['ticket_type'] ?></h3>
 <h3><?php print $offer['price'] ?></h3>
