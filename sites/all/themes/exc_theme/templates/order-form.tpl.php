c1074c588c7ed1fbd8f974ba5436adb317ee806f
Kiselev Dmitry <kiselev.kala4ek@gmail.com>
2014-08-25 23:14:45 +0700

    Process

--- sites/all/themes/exc_theme/templates/order-form.tpl.php
+++ sites/all/themes/exc_theme/templates/order-form.tpl.php
@@ -1,19 +1,19 @@
-<h1>
+<h2>
   <?php print t('Parameters of your order'); ?>
-</h1>
+</h2>
 <?php print render($form['venue']); ?>
 <?php print render($form['language']); ?>
 
-<h1>
+<h2>
   <?php print t('Your contact details');?>
-</h1>
+</h2>
 <?php print render($form['name']);?>
 <?php print render($form['email']);?>
 <?php print render($form['phone']);?>
 
-<h1>
+<h2>
   <?php print t('The contact details of the tourist'); ?>
-</h1>
+</h2>
 <?php print render($form['tourist_name']);?>
 <?php print render($form['tourist_email']);?>
 <?php print render($form['tourist_phone']);?>
