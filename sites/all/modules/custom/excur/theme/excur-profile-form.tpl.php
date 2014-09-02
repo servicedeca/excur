7c4fcc040b1e6aa737665bf968288cb0dc21774b
Your Name <you@example.com>
2014-09-02 18:52:41 +0700

    #6

--- sites/all/modules/custom/excur/theme/excur-profile-form.tpl.php
+++ sites/all/modules/custom/excur/theme/excur-profile-form.tpl.php
@@ -1,3 +1,23 @@
-<?php print $form['account']['name'];?>
-<?php print $form['current_pass'];?>
-<?php print drupal_render_children($form); ?>
+<div class="tabbable">
+  <ul class="nav nav-tabs">
+    <li class="active"><a href="#pane1" data-toggle="tab"><?php print t('General'); ?></a></li>
+    <li><a href="#pane2" data-toggle="tab"><?php print t('Change Password');?></a></li>
+    <li><a href="#pane3" data-toggle="tab"></a></li>
+    <li><a href="#pane4" data-toggle="tab"></a></li>
+  </ul>
+  <div class="tab-content">
+    <div id="pane1" class="tab-pane active">
+      <?php print drupal_render_children($form); ?>
+    </div>
+    <div id="pane2" class="tab-pane">
+      <?php print render($form['account']['pass'])?>
+      <?php print render($form['actions']['submit'])?>
+    </div>
+    <div id="pane3" class="tab-pane">
+
+    </div>
+    <div id="pane4" class="tab-pane">
+
+    </div>
+  </div>
+</div>
