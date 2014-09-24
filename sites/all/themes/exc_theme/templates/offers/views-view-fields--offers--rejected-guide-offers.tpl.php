3b28057392ea0806f4957db9b872cfc0ae8e5cef
Your Name <you@example.com>
2014-09-24 17:04:18 +0700

    process

--- sites/all/themes/exc_theme/templates/offers/views-view-fields--offers--rejected-guide-offers.tpl.php
+++ sites/all/themes/exc_theme/templates/offers/views-view-fields--offers--rejected-guide-offers.tpl.php
@@ -1,24 +1,20 @@
-<div class="container">
+<div class="booking-card">
   <div class="row">
-    <div class="span9 booking-card">
-      <div class="row">
-        <div class="span2 booking-card-image">
-          <?php if (!empty($image)): ?>
-            <?php print $image; ?>
-          <?php endif; ?>
-        </div>
-        <div class="span3 booking-card-body">
-          <h2><?php print $title; ?></h2>
-          <h3><i class="fa fa-th-list"></i>&nbsp<b><?php print t('Order number'); ?>:</b>&nbsp<?php print $id; ?>
-          </h3>
-          <h3><i class="fa fa-calendar"></i>&nbsp<b><?php print t('Date'); ?>:</b>&nbsp<?php print $date; ?>
-          </h3>
-        </div>
-        <div class="span4 booking-card-status">
-          <?php print $fields['status']->content; ?>
-          <div class="tag order-card-button"><?php print $details; ?></div>
-        </div>
-      </div>
+    <div class="span2 booking-card-image">
+      <?php if (!empty($image)): ?>
+        <?php print $image; ?>
+      <?php endif; ?>
+    </div>
+    <div class="span3 booking-card-body">
+      <h2><?php print $title; ?></h2>
+      <h3><i class="fa fa-th-list"></i>&nbsp<b><?php print t('Order number'); ?>:</b>&nbsp<?php print $id; ?>
+      </h3>
+      <h3><i class="fa fa-calendar"></i>&nbsp<b><?php print t('Date'); ?>:</b>&nbsp<?php print $date; ?>
+      </h3>
+    </div>
+    <div class="span4 booking-card-status">
+      <?php print $fields['status']->content; ?>
+      <div class="tag order-card-button"><?php print $details; ?></div>
     </div>
   </div>
 </div>
