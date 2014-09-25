4faa89087edf613624d336d3058adb9a5b04fad9
Your Name <you@example.com>
2014-09-25 19:32:21 +0700

    process

--- sites/all/themes/exc_theme/templates/node/node--page-full.tpl.php
+++ sites/all/themes/exc_theme/templates/node/node--page-full.tpl.php
@@ -4,7 +4,20 @@
  * @file
  */
 ?>
-<article id="node-<?php print $node->nid; ?>">
-  <h1><?php print $title; ?></h1>
-  <?php print render($content); ?>
-</article>
+<div class="row">
+  <div class="span12">
+    <h1><?php print $title; ?></h1>
+  </div>
+  <div class="span3">
+    <ul class="nav nav-tabs nav-stacked">
+      <?php foreach($menu as $item): ?>
+        <li>
+          <?php print $item ?>
+        </li>
+      <?php endforeach; ?>
+    </ul>
+  </div>
+  <div class="span9">
+    <?php print render($content); ?>
+  </div>
+</div>
