c366b31f095033d2c1ba0f462a4936175a420d32
Kiselev Dmitry <kiselev.kala4ek@gmail.com>
2014-10-05 21:43:31 +0700

    Process

--- sites/all/modules/custom/excur/theme/excur-service-node-form.tpl.php
+++ sites/all/modules/custom/excur/theme/excur-service-node-form.tpl.php
@@ -1,4 +1,8 @@
-<?php hide($form['step_1']['language']); ?>
-<?php hide($form['actions']); ?>
-<?php print drupal_render_children($form);?>
-<?php print render($form['actions']); ?>
+<?php if ($user->uid == 1): ?>
+  <?php print drupal_render_children($form);?>
+<?php else: ?>
+  <?php hide($form['step_1']['language']); ?>
+  <?php hide($form['actions']); ?>
+  <?php print drupal_render_children($form);?>
+  <?php print render($form['actions']); ?>
+<?php endif; ?>
