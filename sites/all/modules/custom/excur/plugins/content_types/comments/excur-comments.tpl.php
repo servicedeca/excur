2c55180b2662edb4fe65357e39c30feeddac43a9
Kiselev Dmitry <kiselev.kala4ek@gmail.com>
2014-10-07 14:46:22 +0700

    process

--- sites/all/modules/custom/excur/plugins/content_types/comments/excur-comments.tpl.php
+++ sites/all/modules/custom/excur/plugins/content_types/comments/excur-comments.tpl.php
@@ -6,4 +6,9 @@
     <?php print t('To write a comment, please enter the ticket number.');?>
     <?php print $form; ?>
   <?php endif ?>
+  <?php if(!empty($login)): ?>
+  <div class="offer-login">
+    <?php print $login; ?>
+  </div>
+  <?php endif ?>
 </div>
