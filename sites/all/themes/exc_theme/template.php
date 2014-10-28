53c429938ff0c4020f4e9adb3460237a0000133a
Kiselev Dmitry <kiselev.kala4ek@gmail.com>
2014-10-28 22:33:49 +0600

    Hotfix

--- sites/all/themes/exc_theme/template.php
+++ sites/all/themes/exc_theme/template.php
@@ -50,7 +50,7 @@ function exc_theme_preprocess_page(&$vars, $hook) {
   $vars['footer_menu'] = $footer_menu;
 
   // Add currency change form.
-   $vars['currency_switcher'] = drupal_get_form('excur_currency_choice_form');
+  $vars['currency_switcher'] = drupal_get_form('excur_currency_choice_form');
 
   // Get main menu.
   foreach (i18n_menu_translated_tree('main-menu') as $item) {
@@ -194,7 +194,7 @@ function exc_theme_preprocess_node__service_other(&$vars) {
     'title' => $node->title,
   ));
   $vars['currency'] = excur_currency_get_icon($currency);
-  $vars['title'] = l(t($node->title), 'node/'.$node->nid);
+  $vars['title'] = l($node->title, "node/$node->nid");
 }
 
 /**
