52366ae34f816990ccf78f4c488b422893266cc2
Deca Service <servicedeca@gmail.com>
2014-11-29 19:38:57 +0700

    new photo

--- js/js.js
+++ js/js.js
@@ -1,4 +1,30 @@
-   
+  
+    $(document).ready(function() {
+
+      //Sort random function
+      function random(owlSelector){
+        owlSelector.children().sort(function(){
+            return Math.round(Math.random()) - 0.5;
+        }).each(function(){
+          $(this).appendTo(owlSelector);
+        });
+      }
+
+      $("#owl-demo").owlCarousel({
+        navigation: true,
+        navigationText: [
+        "",
+        ""
+        ],
+        //Call beforeInit callback, elem parameter point to $("#owl-demo")
+        beforeInit : function(elem){
+          random(elem);
+        }
+      });
+    });
+
+
+
 	$(document).ready(function(){
 		$(".reg-item").click(function(){
 			if($(this).attr('class')  ==  'reg-item' ) {
