6e440c68c211a25c54e0ead03badfc5b9804c806
Deca Service <servicedeca@gmail.com>
2014-11-29 10:28:05 +0700

    New main

--- js/js.js
+++ js/js.js
@@ -34,3 +34,17 @@
 		});
 	});
   
+jQuery(document).ready(function(){
+	$objWindow = $(window);
+	$('div[data-type="background"]').each(function(){
+		var $bgObj = $(this);
+		$(window).scroll(function() {
+			var yPos = -($objWindow.scrollTop() / $bgObj.data('speed')); 
+			var coords = '50% '+ yPos + 'px';
+            // Animate the background
+			 $bgObj.css({ backgroundPosition: coords });
+           
+		});
+		
+	});
+});
